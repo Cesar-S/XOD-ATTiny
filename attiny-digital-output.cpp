7db8829bd9c4c1184316e27af8089f4fbd8db807
bitrex <bitrex@earthlink.net>
2018-01-06 19:23:42 -0500

    Update attiny-digital-output.cpp

--- attiny-digital-output.cpp
+++ attiny-digital-output.cpp
@@ -1,24 +1,30 @@
-{{#global}}
+{
+  { #global }
+}
 #include <FixedPoints.h>
 #include <FixedPointsCommon.h>
-{{/global}}
+{
+  { / global }
+}
 
 struct State {
-    int configuredPort = -1;
+  int configuredPort = -1;
 };
 
-{{ GENERATED_CODE }}
+{
+  { GENERATED_CODE }
+}
 
 void evaluate(Context ctx) {
-    State* state = getState(ctx);
-    const int port = (int)getValue<input_PORT>(ctx).getInteger();
-    if (port != state->configuredPort) {
-        ::pinMode(port, OUTPUT);
-        // Store configured port so to avoid repeating `pinMode` call if just
-        // SIG is updated
-        state->configuredPort = port;
-    }
+  State* state = getState(ctx);
+  const int port = (int)getValue<input_PORT>(ctx).getInteger();
+  if (port != state->configuredPort) {
+    ::pinMode(port, OUTPUT);
+    // Store configured port so to avoid repeating `pinMode` call if just
+    // SIG is updated
+    state->configuredPort = port;
+  }
 
-    const bool val = getValue<input_SIG>(ctx);
-    ::digitalWrite(port, val);
+  const bool val = getValue<input_SIG>(ctx);
+  ::digitalWrite(port, val);
 }
