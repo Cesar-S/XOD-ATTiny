fb6190d4bf8ecd696afa0ab1cbd3785c2b99708b
bitrex <bitrex@earthlink.net>
2018-01-06 20:39:52 -0500

    Update XODATTiny.h

--- XODATTiny.h
+++ XODATTiny.h
@@ -16,8 +16,8 @@
 
 namespace XODATTiny {
 
-#define INTEGER_BITS (UCHAR_MAX * CHAR_BIT)
-#define FRACTIONAL_BITS ((UCHAR_MAX - 1) * CHAR_BIT)
+#define INTEGER_BITS (CHAR_BIT - 1)
+#define FRACTIONAL_BITS (CHAR_BIT)
 
 template <unsigned Integer, unsigned Fraction>
 struct XODFixedPoint : SFixed<Integer, Fraction> {
