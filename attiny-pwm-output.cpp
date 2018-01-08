7d8267057bd83fc8a13facf4f588cbbe94ff0e34
bitrex <bitrex@earthlink.net>
2018-01-07 19:18:29 -0500

    Update attiny-pwm-output.cpp

--- attiny-pwm-output.cpp
+++ attiny-pwm-output.cpp
@@ -22,15 +22,13 @@ void evaluate(Context ctx) {
   //check bounds for duty cycle input
 
   auto duty = getValue<input_DUTY>(ctx);
-  duty = duty >= Number(1, 0) ? Number(0, 0.99)
-                             : (duty < Number(0, 0)) ? Number(0, 0) : duty;
+  duty = duty >= Number(1, 0) ? Number(0.99)
+                             : (duty < Number(0, 0)) ? Number(0.) : duty;
   
   //Pick off most significant byte of fixed point fraction and
   //send to analogWrite taking uchar
   
-  uint8_t val =
-      (unsigned long long)(duty.getFraction()) &
-      (~0ULL & (~0ULL << ((sizeof(unsigned long long) * CHAR_BIT) - CHAR_BIT)));
-
+  uint8_t val = duty.getFraction();
+  
   ::analogWrite(port, val);
 }
